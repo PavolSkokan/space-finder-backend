08f5217c12036c5f4e846de8e84fb94382b92a87
PavolSkokan <78729085+PavolSkokan@users.noreply.github.com>
2022-03-07 17:10:36 +0100

    Event Logs and Roles

--- Infrastructure/SpaceStack.ts
+++ Infrastructure/SpaceStack.ts
@@ -6,6 +6,7 @@ import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway'
 import { HealthCheckProtocol } from 'aws-cdk-lib/aws-globalaccelerator';
 import { GenericTable } from './GenericTable';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs'
+import { PolicyStatement } from 'aws-cdk-lib/aws-iam'
 
 export class SpaceStack extends Stack {
     
@@ -20,19 +21,18 @@ export class SpaceStack extends Stack {
         super(scope, id, props)
 
 
-        const helloLambda = new LambdaFunction(this, 'helloLambda', {
-            runtime: Runtime.NODEJS_14_X,
-            code: Code.fromAsset(join(__dirname, '..', 'services', 'hello')),
-            handler: 'hello.main'
-        })
-
         const helloLambdaNodeJs = new NodejsFunction(this, 'helloLambdaNodeJs', {
             entry: (join(__dirname, '..', 'services', 'node-lambda', 'hello.ts')),
             handler: 'handler'
         });
 
+        const s3ListPolicy = new PolicyStatement();
+        s3ListPolicy.addActions('s3:ListAllMyBuckets');
+        s3ListPolicy.addResources('*')
+        helloLambdaNodeJs.addToRolePolicy(s3ListPolicy)
+
         //Hello Api Lambda integration:
-        const helloLambdaIntegration = new LambdaIntegration(helloLambda) 
+        const helloLambdaIntegration = new LambdaIntegration(helloLambdaNodeJs) 
         const helloLambdaResource = this.api.root.addResource('hello')   
         helloLambdaResource.addMethod('GET',helloLambdaIntegration)
     }
