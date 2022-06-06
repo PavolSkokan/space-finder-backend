7913ef8044e6e16a005079169a8cee645452fafd
PavolSkokan <78729085+PavolSkokan@users.noreply.github.com>
2022-06-06 19:08:39 +0200

    40. lesson delete

--- Infrastructure/SpaceStack.ts
+++ Infrastructure/SpaceStack.ts
@@ -49,9 +49,9 @@ export class SpaceStack extends Stack {
         //Spaces API integration
         const spaceResource = this.api.root.addResource('spaces');
         spaceResource.addMethod('POST', this.spacesTable.createLambdaIntegration);
-        spaceResource.addMethod('GET', this.spacesTable.readLambdaIntegration),
-        spaceResource.addMethod('PUT', this.spacesTable.updateLambdaIntegration),
-        spaceResource.addMethod('DELETE', this.spacesTable.deleteLambdaIntegration)
+        spaceResource.addMethod('GET', this.spacesTable.readLambdaIntegration);
+        spaceResource.addMethod('PUT', this.spacesTable.updateLambdaIntegration);
+        spaceResource.addMethod('DELETE', this.spacesTable.deleteLambdaIntegration);
         
     }
 
